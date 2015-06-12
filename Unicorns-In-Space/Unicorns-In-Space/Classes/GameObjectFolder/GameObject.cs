e1427cb6a0fc9a1d68a6b1025b720f218f16978d
Joshua Endert <joshua95@gmx.net>
2015-06-12 21:27:47 +0200

    sadfsafas

--- Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/GameObject.cs
+++ Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/GameObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SFML.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Unicorns_In_Space.Classes.GameObjectFolder
 {
-    class GameObject
+    abstract class GameObject
     {
+        public Sprite sprite { get; set; }
+        public Vec2 position { get; set; }
+        public HitBox hitBox { get; protected set; }
+        protected bool _isAlive = true;
+        public bool isAlive { get { return _isAlive; } set { _isAlive = value; } }
+
     }
 }
--- Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/GameObjectHandler.cs
+++ Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/GameObjectHandler.cs
@@ -8,12 +8,52 @@ using System.Threading.Tasks;
 
 namespace Unicorns_In_Space.Classes.GameObjectFolder
 {
-    abstract class GameObjectHandler
+    class GameObjectHandler
     {
-        public Sprite sprite { get; set; }
-        public Vector2f position { get; set; }
-        protected bool _isAlive = true;
-        public bool isAlive { get { return _isAlive; } set { _isAlive = value; } }
+        public static List<GameObject> gameObjectList { get; set; }
 
+        public GameObjectHandler()
+        {
+            gameObjectList = new List<GameObject>();
+        }
+
+        public static void Add(GameObject obj)
+        {
+            gameObjectList.Add(obj);
+        }
+
+        public void Deleate()
+        {
+            foreach (GameObject gObj in gameObjectList)
+                gObj.Kill();
+        }
+
+        public void Update(GameTime gameTIme)
+        {
+            gameObjectList.Sort();
+
+            for(int i =0; i< gameObjectList.Count; ++i)
+            {
+                if(!gameObjectList[i].isAlive)
+                {
+                    gameObjectList.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    gameObjectList[i].indexObjectList = i;
+                    gameObjectList[i].Update(gameTime);
+                }
+            }
+        }
+
+
+        public void Draw(RenderWindow window)
+        {
+            foreach (GameObject gObj in gameObjectList)
+            {
+                gObj.Draw(window);
+            }
+        }
     }
 }
