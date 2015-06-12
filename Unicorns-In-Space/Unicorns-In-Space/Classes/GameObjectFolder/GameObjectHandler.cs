7a6c9d257a155ea4dee8bf0ed411890e8b213587
Joshua Endert <joshua95@gmx.net>
2015-06-12 20:48:12 +0200

    sdfkbsfhksdj

---
+++ Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/GameObjectHandler.cs
@@ -0,0 +1,16 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unicorns_In_Space.Classes.GameObjectFolder
+{
+    abstract class GameObjectHandler
+    {
+        public Sprite sprite { get; set; }
+        public Vector2f position { get; set; }
+    }
+}
