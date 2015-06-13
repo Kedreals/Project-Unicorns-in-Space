12c1f925c0b184e237c0ded7cad5ca0caa05c417
Kedreals <ebert.mueden1@freenet.de>
2015-06-13 15:34:37 +0200

    SO MUCH PLAYER TOO MANY WOW

--- Unicorns-In-Space/Unicorns-In-Space/Classes/GameStatesFolder/MainMenu.cs
+++ Unicorns-In-Space/Unicorns-In-Space/Classes/GameStatesFolder/MainMenu.cs
@@ -17,7 +17,7 @@ namespace Unicorns_In_Space
 
         Sprite Background;
         Sprite FlashyTitel;
-        Sprite point;
+        CircleShape point;
 
         Shader flashShader;
         Shader fadeShade;
@@ -26,6 +26,8 @@ namespace Unicorns_In_Space
         RenderStates fade;
 
         RenderTexture renderTexture;
+
+        bool newPointsSet = true;
 
         public void Initialize()
         {
@@ -47,9 +49,10 @@ namespace Unicorns_In_Space
 
             renderTexture = new RenderTexture(Game.WindowWidth, Game.WindowHeight);
             renderTexture.Display();
+            fadeShade.SetParameter("overlay", renderTexture.Texture);
 
-            point = new Sprite(new CircleShape(2).Texture);
-            point.Color = Color.White;
+            point = new CircleShape(2);
+            point.FillColor = Color.White;
         }
         
 
@@ -58,20 +61,10 @@ namespace Unicorns_In_Space
         {
             flashShader.SetParameter("time", (float)gameTime.TotalTime.TotalSeconds * 5f);
             fadeShade.SetParameter("time", (float)gameTime.TotalTime.TotalSeconds);
-            fadeShade.SetParameter("overlay", renderTexture.Texture);
 
-            if (Math.Sin(gameTime.TotalTime.Seconds) == 1.0)
+            if (Math.Sin(gameTime.TotalTime.Seconds) * 0.5 + 0.5 <= 0.01)
             {
-                Background.Texture = renderTexture.Texture;
-                renderTexture.Clear(Color.Black);
-
-                for (int i = 0; i < 10; ++i)
-                {
-                    //point.Position = new Vec2(r.Next((int)Game.WindowWidth), r.Next((int)Game.WindowHeight));
-                    point.Position = new Vec2(100, 100);
-                    renderTexture.Draw(point);
-                }
-                
+                newPointsSet = true;
             }
 
             return EnumGameStates.mainMenu;
@@ -79,6 +72,22 @@ namespace Unicorns_In_Space
 
         public void Draw(RenderWindow window)
         {
+
+            if (newPointsSet)
+            {
+                renderTexture.Clear(Color.Black);
+
+                for (int i = 0; i < 117; ++i)
+                {
+                    point.Position = new Vec2(r.Next((int)Game.WindowWidth), r.Next((int)Game.WindowHeight));
+
+                    renderTexture.Draw(point);
+                }
+                renderTexture.Draw(point);
+
+                newPointsSet = false;
+            }
+
             renderTexture.Display();
             window.Draw(Background, fade);
 
