d3d499b769e9abcb0d4c73c62664885aa6eee5ba
Joshua Endert <joshua95@gmx.net>
2015-06-14 12:26:14 +0200

    fgdsfsadsgfasfa

--- Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/EntityFolder/EnemyFolder/Enemy.cs
+++ Unicorns-In-Space/Unicorns-In-Space/Classes/GameObjectFolder/EntityFolder/EnemyFolder/Enemy.cs
@@ -27,16 +27,16 @@ namespace Unicorns_In_Space
             switch (rando)
             {
                 case 0:
-                    r = (d) => { return (float)Math.Sin(d * 2); };
+                    r = (d) => { return (float)Math.Sin(d); };
                     break;
                 case 1:
-                    r = (d) => { return (float)Math.Cos(d * 2); };
+                    r = (d) => { return (float)Math.Cos(d); };
                     break;
                 case 2:
-                    r = (d) => { return -(float)Math.Cos(d * 2); };
+                    r = (d) => { return -(float)Math.Cos(d); };
                     break;
                 case 3:
-                    r = (d) => { return -(float)Math.Sin(d * 2); };
+                    r = (d) => { return -(float)Math.Sin(d); };
                     break;
                 default:
                     r = (d) => { return 0.0f; };
@@ -61,7 +61,7 @@ namespace Unicorns_In_Space
 
         public void RandomMovement()
         {
-            Movement = new Vec2(-1, r(stop.Elapsed.Seconds + 60*(stop.Elapsed.Minutes + 60*stop.Elapsed.Hours))/MovementSpeed);
+            Movement = new Vec2(-1, r(stop.Elapsed.Seconds + 60*(stop.Elapsed.Minutes + 60*stop.Elapsed.Hours)));
         }
 
         public override void Update(GameTime gameTime)
